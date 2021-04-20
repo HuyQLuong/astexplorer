f1ca36733b963fcab09e6d58dac0fa440babed16
Phan Truong Thanh <thanh@quod.ai>
2021-04-20 09:33:11 +0700

    Add color

--- website/src/parsers/ruby/index.js
+++ website/src/parsers/ruby/index.js
@@ -1,5 +1,7 @@
-
+import 'codemirror/mode/ruby/ruby';
 export const id = 'ruby';
 export const displayName = 'ruby';
 export const fileExtension = 'rb';
-export const mimeTypes = [];
+
+export const mimeTypes = [ 'text/x-ruby'];
+export const editorMode = 'text/x-ruby';
