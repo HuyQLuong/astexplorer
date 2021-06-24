54600589753a35f06cb8ea65065111fcbbceb205
Phan Truong Thanh <thanh@quod.ai>
2021-06-24 19:36:53 +0700

    Update server.js

--- website/src/server.js
+++ website/src/server.js
@@ -1,15 +1,41 @@
-const express = require('express')
+
+import {getParserByID} from './parsers';
+
+import express from 'express';
 const app = express()
-const port = 3000
+const port = 3300
+
+function parse(parser, code, parserSettings) {
+    if (!parser._promise) {
+        parser._promise = new Promise(parser.loadParser);
+    }
+    return parser._promise.then(
+        realParser => parser.parse(
+            realParser,
+            code,
+            parserSettings || parser.getDefaultOptions(),
+        ),
+    );
+}
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+app.get('/health', (req, res) => {
+    res.send('OK')
+})
 
 app.post('/parse', (req, res) => {
-
+    const request_object = req.body;
+    const snippet = request_object.code;
+    const parser_name = request_object.parser_name;
+    const parser_setting = request_object.parser_setting;
+    const parser = getParserByID(parser_name)
+    res.json(parse(parser, snippet,parser_setting))
 })
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
+
+export default app;
